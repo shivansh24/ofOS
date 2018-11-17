1189d82ac8121384d549c65fc435040febc60baf
Anmol Gautam <tarptaeya@gmail.com>
2018-11-18 02:07:35 +0530

    changed dir str

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,17 +1,29 @@
 [org 0x7c00]
 
-mov bx, WelcomeMessage
-call print_string
-call print_endline
+    mov bp, 0x9000
+    mov sp, bp
 
-mov dx, 0xbabe
-call print_hex
-jmp $
+    mov bx, MSG_REAL_MODE
+    call print_string
 
-%include "print.asm"
+    call switch_to_pm
+    jmp $
 
-WelcomeMessage:
-    db 'Welcome to ofOS', 0
+%include "print_string_16.asm"
+%include "gdt_32.asm"
+%include "print_string_pm_32.asm"
+%include "switch_to_pm_32.asm"
 
-times 510 - ($ - $$) db 0x0
+[bits 32]
+BEGIN_PM:
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm
+    jmp $
+
+
+MSG_REAL_MODE db "Started in 16-bit Real Mode", 0 
+MSG_PROT_MODE db "Successfully switched to 32-bit Protected Mode :)", 0
+
+; Bootsector Padding
+times 510-($-$$) db 0
 dw 0xaa55
